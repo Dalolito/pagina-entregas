d753dab49a2d10da4ff2da8fd419b9d7e27a64fd
Dalolito <70274865+Dalolito@users.noreply.github.com>
2021-04-17 18:43:08 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 var mfoto=document.getElementById('fotoyn');
+var mpag3=document.getElementById('paag3');
 var mpag2=document.getElementById('paag2');
 var mpag1=document.getElementById('inicio');
 var mostrar=function () {
@@ -9,7 +10,14 @@ var pag2=function () {
 	mpag1.className="oculto";
 	mfoto.className="oculto";
 }
+var pag3=function () {
+	mpag3.className="visible";
+	mpag2.className="oculto";
+	mpag1.className="oculto";
+	mfoto.className="oculto";
+}
 var devolver=function () {
+	mpag3.className="oculto";
 		mpag2.className="oculto";
 	mpag1.className="visible";
 	mfoto.className="oculto";
